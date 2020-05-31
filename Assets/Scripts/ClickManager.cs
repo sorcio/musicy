3911f84de5f4301e6bc6cf3eec1f98216cb32fbd
Davide Rizzo <sorcio@gmail.com>
2020-06-01 01:28:17 +0200

    musicy

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+
+    public Note noteSoundPrefab;
+    public float interval;
+
+    private Note noteSound;
+    ParticleSystem sparks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sparklinessPrefab = Resources.Load<GameObject>("Sparkliness");
+        var sparkliness = Instantiate(sparklinessPrefab);
+        sparks = sparkliness.GetComponent<ParticleSystem>();
+
+        this.noteSound = Instantiate(noteSoundPrefab);
+        int octave = ExtractOctaveFromName(this.transform.parent.name);
+        this.noteSound.Pitch = Mathf.Pow(2.0f, (octave - 2) + (interval / 12));
+    }
+
+    static int ExtractOctaveFromName(string name)
+    {
+        string[] parts = name.Split(new char[] {' '}, 2);
+        if (parts[0] == "Octave")
+        {
+            int result;
+            if (int.TryParse(parts[1], out result))
+            {
+                return result;
+            }
+        }
+        return 2;  // well this is the default octave for some reason
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+                if (objectHit == transform)
+                {
+                    this.MouseHit(hit);
+                    return;
+                }
+            }
+        }
+        this.MouseNotHit();
+    }
+
+    void MouseHit(RaycastHit hit)
+    {
+        if (this.noteSound.Press())
+        {
+            this.sparks.Play();
+        }
+        this.sparks.transform.position = hit.point;
+    }
+
+    void MouseNotHit()
+    {
+        this.noteSound.Release();
+        this.sparks.Stop();
+    }
+}
---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//public enum NoteState
+//{
+//    Off,
+//    Attack,
+//    Delay,
+//    Sustain,
+//    Release,
+//}
+
+public class Note : MonoBehaviour
+{
+    AudioSource audioSource;
+    bool noteOn;
+    float pitch;
+
+    public float attackTime = 0.050f;
+    public float decayTime = 0.100f;
+    public float sustainLevel = 0.5f;
+    public float releaseTime = 0.200f;
+
+    void Start()
+    {
+        this.audioSource = GetComponent<AudioSource>();
+        this.noteOn = false;
+        StartCoroutine(PlayNoteCoroutine());
+    }
+
+    public float Pitch
+    {
+        get => this.pitch;
+        set {
+            this.pitch = value;
+            if (audioSource != null) audioSource.pitch = pitch;
+        }
+    }
+
+    public bool Press()
+    {
+        var ret = !this.noteOn;
+        this.noteOn = true;
+        return ret;
+    }
+
+    public bool Release()
+    {
+        var ret = this.noteOn;
+        this.noteOn = false;
+        return ret;
+    }
+
+    public bool IsNoteOn()
+    {
+        return this.noteOn;
+    }
+
+    public bool IsNoteOff()
+    {
+        return !this.noteOn;
+    }
+
+    IEnumerator PlayNoteCoroutine()
+    {
+        this.audioSource.volume = 0;
+        this.audioSource.loop = true;
+        this.audioSource.pitch = pitch;
+        this.audioSource.Play();
+        while (true)
+        {
+            yield return new WaitUntil(IsNoteOn);
+            // Attack
+            Debug.Log("attack");
+            while (IsNoteOn() && this.audioSource.volume < 1.0f)
+            {
+                this.audioSource.volume += (1.0f / attackTime) * Time.deltaTime;
+                yield return null;
+            }
+            // Decay
+            Debug.Log("decay");
+            while (IsNoteOn() && this.audioSource.volume > sustainLevel)
+            {
+                this.audioSource.volume -= ((1f - sustainLevel) / decayTime) * Time.deltaTime;
+                yield return null;
+            }
+            // Sustain
+            Debug.Log("sustain");
+            if (IsNoteOn())
+            {
+                yield return new WaitUntil(IsNoteOff);
+            }
+            // Release
+            Debug.Log("release");
+            while (IsNoteOff() && this.audioSource.volume > 0)
+            {
+                this.audioSource.volume -= ((sustainLevel) / releaseTime) * Time.deltaTime;
+                yield return null;
+            }
+        };
+    }
+}
